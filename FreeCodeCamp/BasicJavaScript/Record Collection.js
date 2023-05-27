6dfc144298fea53c0278719a8ca375cada37eb5e
PopovRY <rupopov91@gmail.com>
2023-05-27 20:29:21 +1000

    up

--- FreeCodeCamp/BasicJavaScript/Record Collection.js
+++ FreeCodeCamp/BasicJavaScript/Record Collection.js
@@ -21,12 +21,12 @@ const recordCollection = {
 
 // Only change code below this line
 function updateRecords(records, id, prop, value) {
-    if (value === " ") {
+    if (value === "") {
         delete records[id][prop];
     } else if (prop !== "tracks" && value !== "") {
         records[id][prop] = value;
-    } else if (prop === "tracks" && value !=="") {
-        if (records[id].hasOwnProperty("tracks") === false ) {
+    } else if (prop === "tracks" && value !== "") {
+        if (records[id].hasOwnProperty("tracks") === false) {
             records[id][prop] = [];
         }
         records[id][prop].push(value);
