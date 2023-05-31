eb6e359145105b07b589fb73dcbaa31f5808fd55
PopovRY <rupopov91@gmail.com>
2023-05-31 17:38:59 +1000

    up

---
+++ FreeCodeCamp/BasicCSS/Use Hex Code to Mix Colors.js
@@ -0,0 +1,22 @@
+<style>
+    .red-text {
+    color: #FF0000;
+}
+    .green-text {
+    color: #00FF00;
+}
+    .dodger-blue-text {
+    color: #1E90FF;
+}
+    .orange-text {
+    color: #FFA500;
+}
+</style>
+
+<h1 className="red-text">I am red!</h1>
+
+<h1 className="green-text">I am green!</h1>
+
+<h1 className="dodger-blue-text">I am dodger blue!</h1>
+
+<h1 className="orange-text">I am orange!</h1>
