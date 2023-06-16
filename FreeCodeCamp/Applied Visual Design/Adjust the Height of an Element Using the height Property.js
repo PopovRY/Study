e2c47fd4d8fd971d76489a8c9c3ba151a0f03394
PopovRY <rupopov91@gmail.com>
2023-06-16 14:55:13 +1000

    up

---
+++ FreeCodeCamp/Applied Visual Design/Adjust the Height of an Element Using the height Property.js
@@ -0,0 +1,37 @@
+<style>
+    h4 {
+    text - align: center;
+    height: 25px;
+
+}
+    p {
+    text - align: justify;
+}
+    .links {
+    margin - right: 20px;
+    text-align: left;
+}
+    .fullCard {
+    width: 245px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    margin: 10px 5px;
+    padding: 4px;
+}
+    .cardContent {
+    padding: 10px;
+}
+</style>
+<div className="fullCard">
+    <div className="cardContent">
+        <div className="cardText">
+            <h4>Google</h4>
+            <p>Google was founded by Larry Page and Sergey Brin while they were Ph.D. students at Stanford
+                University.</p>
+        </div>
+        <div className="cardLinks">
+            <a href="https://en.wikipedia.org/wiki/Larry_Page" target="_blank" className="links">Larry Page</a>
+            <a href="https://en.wikipedia.org/wiki/Sergey_Brin" target="_blank" className="links">Sergey Brin</a>
+        </div>
+    </div>
+</div>
---
+++ FreeCodeCamp/Applied Visual Design/Create Visual Balance Using the text-align Property.js
@@ -0,0 +1,36 @@
+<style>
+    h4 {
+    text - align: center;
+
+}
+    p {
+    text - align: justify;
+
+}
+    .links {
+    margin - right: 20px;
+
+}
+    .fullCard {
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    margin: 10px 5px;
+    padding: 4px;
+}
+    .cardContent {
+    padding: 10px;
+}
+</style>
+<div className="fullCard">
+    <div className="cardContent">
+        <div className="cardText">
+            <h4>Google</h4>
+            <p>Google was founded by Larry Page and Sergey Brin while they were Ph.D. students at Stanford
+                University.</p>
+        </div>
+        <div className="cardLinks">
+            <a href="https://en.wikipedia.org/wiki/Larry_Page" target="_blank" className="links">Larry Page</a>
+            <a href="https://en.wikipedia.org/wiki/Sergey_Brin" target="_blank" className="links">Sergey Brin</a>
+        </div>
+    </div>
+</div>
