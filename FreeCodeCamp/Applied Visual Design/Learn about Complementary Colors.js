7d67aa56767ff1e4ea1c3255d5dca49b3cff8a80
PopovRY <rupopov91@gmail.com>
2023-06-25 21:03:30 +1000

    up

---
+++ FreeCodeCamp/Applied Visual Design/Learn about Complementary Colors.js
@@ -0,0 +1,18 @@
+<style>
+    body {
+    background - color: #FFFFFF;
+}
+    .blue {
+    background - color: blue;
+}
+    .yellow {
+    background - color: yellow;
+}
+    div {
+    display: inline-block;
+    height: 100px;
+    width: 100px;
+}
+</style>
+<div className="blue"></div>
+<div className="yellow"></div>
